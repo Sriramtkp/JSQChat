e6be1b79a6b80141c28506c82a13da6e20bac2ef
Sriram Rajendran <sriramrajendrantkp@yahoo.in>
2016-10-20 16:09:46 +0530

    Part-12   Recent cell Update for the Timestamp

--- JSQChat/RecentTableViewCell.swift
+++ JSQChat/RecentTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class RecentTableViewCell: UITableViewCell {
-
+    let backendShardInstance = Backendless.sharedInstance()
+    
     
     
     @IBOutlet weak var avatarImageCell: UIImageView!
@@ -22,9 +23,7 @@ class RecentTableViewCell: UITableViewCell {
     
     @IBOutlet weak var counterLabel: UILabel!
     
-    
-    
-    override func awakeFromNib() {
+       override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
@@ -35,4 +34,89 @@ class RecentTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+      func bindDataFromCell(recentFromCell: NSDictionary) {
+        
+        self.avatarImageCell.layer.cornerRadius = self.avatarImageCell.frame.size.width/2
+        self.avatarImageCell.layer.masksToBounds = true
+        
+        self.avatarImageCell.image = UIImage(named: "avatarPlaceholder")
+        
+        //run a query to avatar details from Backendless
+        
+        let withUserID = (recentFromCell.objectForKey("withUserUserID") as? String)!
+        
+        // get the user's avatar
+        let whereClauseObj = "objectID = '\(withUserID)"
+        let dataQuery = BackendlessDataQuery()
+        dataQuery.whereClause = whereClauseObj
+        let dataStoreObj = backendShardInstance.persistenceService.of(BackendlessUser.ofClass())
+        dataStoreObj.find(dataQuery, response: { (users: BackendlessCollection!) in
+            
+            let withUserObj = users.data.first as! BackendlessUser
+            //here withUserObj is to get get Avatar of the user 
+            
+            
+        }) { (fault: Fault!) in
+            
+            print(".....Faults;;;;\(fault)")
+        }
+        
+        nameLabel.text = recentFromCell["withUserUsername"] as? String
+        lastMessageLabel.text = recentFromCell["lastMessage"] as? String
+        
+        counterLabel.text = ""
+        
+        if (recentFromCell["counter"] as? Int)! != 0 {
+            counterLabel.text = "\(recentFromCell["counter"])!"
+        }
+        
+        //date
+        timerLabel.text = ""
+        
+        
+        
+    }
+    
+    //MARK: date,time,seconds
+    func timeElapsed(seconds: NSTimeInterval) -> String {
+        var elapsedStr : String?
+        
+        if (seconds < 60) {
+            elapsedStr = "Just now"
+        }
+        else if (seconds < 60 * 60){
+            let minutes = Int(seconds / 60)
+            var minText = "min"
+            
+            if minutes > 1 {
+                minText = "mins"
+            }
+            elapsedStr = "\(minutes) \(minText)"
+        }
+        else if (seconds < 24 * 60 * 60){
+            let hours = Int(seconds / 60 * 60)
+            var hoursText = "hour"
+            
+            if hours > 1 {
+                hoursText = "hours"
+            }
+            elapsedStr = "\(hours) \(hoursText)"
+        }
+        else {
+            let day = Int(seconds / 24 * 60 * 60)
+            var dayText = "hour"
+            
+            if day > 1 {
+                dayText = "hours"
+            }
+            elapsedStr = "\(day) \(dayText)"
+        }
+
+        
+        
+        
+    return elapsedStr!
+    }
+    
+    
 }
